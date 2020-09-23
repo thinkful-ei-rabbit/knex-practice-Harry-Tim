e1c9197c97a71eedc69e44c1c5862d07ae639138
Harrison Winkler <harryewinkler@thinkful.com>
2020-09-23 14:43:50 -0400

    added Test Data

--- test/shoppingListService.test.js
+++ test/shoppingListService.test.js
@@ -1,9 +1,35 @@
 const ShoppingListService = require('../src/shopping-list-service');
 const knex = require('knex');
-require('dotenv').config();
 
 describe('Shopping List Service object', function () {
     let db;
+
+    let testData = [
+            {
+              item_id: 1,
+              item_name: 'Fish tricks',
+              price: '13.10',
+              date_added: '2020-09-01T23:33:00.501Z',
+              checked: false,
+              category: 'Main'
+            },
+            {
+              item_id: 2,
+              item_name: 'Not Dogs',
+              price: '4.99',
+              date_added: '2020-09-01T23:33:00.501Z',
+              checked: true,
+              category: 'Snack'
+            },
+            {
+              item_id: 3,
+              item_name: 'Bluffalo Wings',
+              price: '5.50',
+              date_added: '2020-09-01T23:33:00.501Z',
+              checked: false,
+              category: 'Snack'
+            }
+    ];
     
     before(() => {
         db = knex({
@@ -13,6 +39,8 @@ describe('Shopping List Service object', function () {
     });
 
     describe('This is for all the tests for the GET items', function () {
-        it('getAllItems', function () )
+        it('should return the table from getAllItems()', function () {
+
+        });
     })
 }
